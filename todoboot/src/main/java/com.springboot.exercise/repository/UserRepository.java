89ae017003a0684a506b01eafc30a80fe6d8b294
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-03-21 17:19:30 +0100

    Spring todo thymleaf is  added

---
+++ todoboot/src/main/java/com.springboot.exercise/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.exercise.repository;
+
+import com.springboot.exercise.model.User;
+import org.springframework.data.repository.CrudRepository;
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
