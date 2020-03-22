6d61e66d7d400ac7a693ca2993bd04aaf107743c
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-03-23 00:02:20 +0100

    add spring boot admin server

--- spring-boot-admin/src/main/java/com/springboot/exercise/springbootadmin/MainApplication.java
+++ spring-boot-admin/src/main/java/com/springboot/exercise/springbootadmin/MainApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.exercise.sba;
+package com.springboot.exercise.springbootadmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
